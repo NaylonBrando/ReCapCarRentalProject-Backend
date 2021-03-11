7be69b01d69a23b7b6ea5d37749bba24b95ca378
Erhan Rohat Turuncu <rohatturuncu@gmail.com>
2021-03-11 22:49:00 +0300

    ilk versiyon

---
+++ DataAccess/InMemory/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.InMemory.InMemory
+{
+    class InMemoryCarDal : ICarDal
+    {
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int carID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
